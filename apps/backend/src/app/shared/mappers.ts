7af1cb758d65c7d48fcd571fe15a5ec2d45036b2
Nikolay Matyasov <nmatyasov@gmail.com>
2023-03-13 12:00:41 +0300

    Added refrehToken in response payload

--- apps/backend/src/app/shared/mappers.ts
+++ apps/backend/src/app/shared/mappers.ts
@@ -1,29 +1,31 @@
-
-import { TaskDto } from "@tasks/dto/task.dto";
-import { TaskModel } from "@tasks/models/task.models";
-import { UserDto } from "@users/dto/user.dto"
+import { TaskDto } from '@tasks/dto/task.dto';
+import { TaskModel } from '@tasks/models/task.models';
+import { UserDto } from '@users/dto/user.dto';
 import { UserModel } from '@users/models/user.model';
 import { Types } from 'mongoose';
 
 export const toUserDto = (data: UserModel): UserDto => {
-  const {_id, username, email, isEmailConfirmed} = data;
+  const { _id, username, email, isEmailConfirmed, refreshToken } = data;
 
   const userDto: UserDto & { _id: Types.ObjectId } = {
     _id,
     username,
     email,
-    isEmailConfirmed
+    isEmailConfirmed,
+    refreshToken,
   };
 
   return userDto;
 };
 
-
 export const toTaskDto = (data: TaskModel): TaskDto => {
-  const {_id, title, description, done} = data;
+  const { _id, title, description, done } = data;
 
   const taskDto: TaskDto = {
-    _id, title, description, done
+    _id,
+    title,
+    description,
+    done,
   };
 
   return taskDto;
